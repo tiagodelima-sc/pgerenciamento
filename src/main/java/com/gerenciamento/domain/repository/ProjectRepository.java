08514a9ac521402abe9f1344708b488f56475c64
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-11-29 21:13:46 -0300

    feat: implementado repositorio do projeto

---
+++ src/main/java/com/gerenciamento/domain/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.gerenciamento.domain.repository;
+
+import com.gerenciamento.domain.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, String> {
+}
