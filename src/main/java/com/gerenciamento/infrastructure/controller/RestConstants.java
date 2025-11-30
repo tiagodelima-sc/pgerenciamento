cb70f925b91adf39da6a9991907790e6ce1f3c42
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-11-30 11:58:53 -0300

    chore: criada classe RestConstants para garantir padronização dos endpoints

---
+++ src/main/java/com/gerenciamento/infrastructure/controller/RestConstants.java
@@ -0,0 +1,8 @@
+package com.gerenciamento.infrastructure.controller;
+
+public final class RestConstants {
+
+    public static final String PATH_PROJECTS = "/projects";
+
+    private RestConstants() {}
+}
