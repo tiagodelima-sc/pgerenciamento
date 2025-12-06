265e053cf0f90ace2db49d67ad463648f4ca0f14
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-06 19:52:36 -0300

    feat: adicionar nova constante.

--- src/main/java/com/gerenciamento/infrastructure/controller/RestConstants.java
+++ src/main/java/com/gerenciamento/infrastructure/controller/RestConstants.java
@@ -3,6 +3,7 @@ package com.gerenciamento.infrastructure.controller;
 public final class RestConstants {
 
     public static final String PATH_PROJECTS = "/projects";
+    public static final String PATH_MEMBERS  = "/members";
 
     private RestConstants() {}
 }
