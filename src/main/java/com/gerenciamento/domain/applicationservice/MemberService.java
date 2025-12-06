667694e8d0f9132fd20edd51768a5f4372d85c93
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-06 19:53:35 -0300

    feat: implementar controller e service responsável pelos tratamentos do membro

---
+++ src/main/java/com/gerenciamento/domain/applicationservice/MemberService.java
@@ -0,0 +1,29 @@
+package com.gerenciamento.domain.applicationservice;
+
+import com.gerenciamento.domain.entity.Member;
+import com.gerenciamento.domain.repository.MemberRepository;
+import com.gerenciamento.infrastructure.dto.SaveMemberDataDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member createMember(SaveMemberDataDTO saveMemberDataDTO) {
+        Member member = Member
+                .builder()
+                .name(saveMemberDataDTO.getName())
+                .email(saveMemberDataDTO.getEmail())
+                .secret(UUID.randomUUID().toString())
+                .deleted(false)
+                .build();
+
+        memberRepository.save(member);
+        return member;
+    }
+}
---
+++ src/main/java/com/gerenciamento/infrastructure/controller/MemberRestResource.java
@@ -0,0 +1,31 @@
+package com.gerenciamento.infrastructure.controller;
+
+import com.gerenciamento.domain.applicationservice.MemberService;
+import com.gerenciamento.domain.entity.Member;
+import com.gerenciamento.infrastructure.dto.MemberDTO;
+import com.gerenciamento.infrastructure.dto.SaveMemberDataDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+import static com.gerenciamento.infrastructure.controller.RestConstants.PATH_MEMBERS;
+
+@RestController
+@RequestMapping(PATH_MEMBERS)
+@RequiredArgsConstructor
+public class MemberRestResource {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<MemberDTO> createMember(@RequestBody @Valid SaveMemberDataDTO saveMemberDataDTO) {
+        Member member = memberService.createMember(saveMemberDataDTO);
+
+        return ResponseEntity
+                .created(URI.create(PATH_MEMBERS + member.getId()))
+                .body(MemberDTO.create(member));
+    }
+}
