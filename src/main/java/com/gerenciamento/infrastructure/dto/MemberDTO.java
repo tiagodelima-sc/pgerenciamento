a37beaeed81c23166f1288d6f4b64fdad26783e1
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-07 15:35:17 -0300

    chore: ajustado o relacionamento dos dtos projeto e membro

--- src/main/java/com/gerenciamento/infrastructure/dto/MemberDTO.java
+++ src/main/java/com/gerenciamento/infrastructure/dto/MemberDTO.java
@@ -1,7 +1,14 @@
 package com.gerenciamento.infrastructure.dto;
 
 import com.gerenciamento.domain.entity.Member;
+import com.gerenciamento.domain.entity.Project;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @Data
 public class MemberDTO {
@@ -10,13 +17,20 @@ public class MemberDTO {
     private final String secret;
     private final String name;
     private final String email;
+    private final Set<String> projectIds;
 
     public static MemberDTO create(Member member) {
         return new MemberDTO(
-                member.getId(),
+                member.getId    (),
                 member.getSecret(),
-                member.getName(),
-                member.getEmail()
+                member.getName  (),
+                member.getEmail (),
+                Optional
+                        .ofNullable(member.getProjects())
+                        .orElse(List.of())
+                        .stream()
+                        .map(Project::getId)
+                        .collect(toSet())
         );
     }
 }
--- src/main/java/com/gerenciamento/infrastructure/dto/ProjectDTO.java
+++ src/main/java/com/gerenciamento/infrastructure/dto/ProjectDTO.java
@@ -1,10 +1,16 @@
 package com.gerenciamento.infrastructure.dto;
 
+import com.gerenciamento.domain.entity.Member;
 import com.gerenciamento.domain.entity.Project;
 import com.gerenciamento.domain.model.ProjectStatus;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @Data
 public class ProjectDTO {
@@ -15,6 +21,7 @@ public class ProjectDTO {
     private final LocalDate initialDate;
     private final LocalDate finalDate;
     private final ProjectStatus status;
+    private final Set<String> memberIds;
 
     public static ProjectDTO create(Project project) {
         return new ProjectDTO(
@@ -23,7 +30,13 @@ public class ProjectDTO {
                 project.getDescription(),
                 project.getInitialDate(),
                 project.getFinalDate(),
-                project.getStatus()
+                project.getStatus(),
+                Optional
+                        .ofNullable(project.getMembers())
+                        .orElse(List.of())
+                        .stream()
+                        .map(Member::getId)
+                        .collect(toSet())
         );
     }
 }
