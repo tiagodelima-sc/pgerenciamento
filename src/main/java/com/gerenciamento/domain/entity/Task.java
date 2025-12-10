1e6f94a86ba7fb3be9a1af39a4e7a0616ef0db1f
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-09 21:24:38 -0300

    feat: adicionar nova entidade de tarefa

---
+++ src/main/java/com/gerenciamento/domain/entity/Task.java
@@ -0,0 +1,37 @@
+package com.gerenciamento.domain.entity;
+
+import com.gerenciamento.domain.model.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.UUID;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = UUID)
+    @Column(name = "id", nullable = false, length = 36)
+    private String id;
+
+    @Column(name = "title", nullable = false, length = 80)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 150)
+    private String description;
+
+    @Column(name = "number_of_days", nullable = false)
+    private Integer numberOfDays;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(STRING)
+    private TaskStatus status;
+}
