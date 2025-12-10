f2cbf6ea230f958daf1942b07e6bad2ff6758546
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-09 21:25:18 -0300

    feat: adicionar novo controller da tarefa

---
+++ src/main/java/com/gerenciamento/infrastructure/controller/TaskRestResource.java
@@ -0,0 +1,56 @@
+package com.gerenciamento.infrastructure.controller;
+
+import com.gerenciamento.domain.applicationservice.TaskService;
+import com.gerenciamento.domain.entity.Task;
+import com.gerenciamento.infrastructure.dto.SaveTaskDataDTO;
+import com.gerenciamento.infrastructure.dto.TaskDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+import static com.gerenciamento.infrastructure.controller.RestConstants.PATH_TASKS;
+
+@RestController
+@RequestMapping(PATH_TASKS)
+@RequiredArgsConstructor
+@SuppressWarnings("unused")
+public class TaskRestResource {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@RequestBody @Valid SaveTaskDataDTO saveTaskDataDTO) {
+        Task task = taskService.createTask(saveTaskDataDTO);
+
+        return ResponseEntity
+                .created(URI.create(PATH_TASKS + task.getId()))
+                .body   (TaskDTO.create(task));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDTO> loadTask(@PathVariable("id") String taskId) {
+        Task task = taskService.loadTask(taskId);
+
+        return ResponseEntity.ok(TaskDTO.create(task));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id") String id) {
+        taskService.deleteTask(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTask(
+            @PathVariable("id") String taskId,
+            @RequestBody @Valid SaveTaskDataDTO saveTaskDataDTO) {
+        Task task = taskService.updateTask(taskId, saveTaskDataDTO);
+
+        return ResponseEntity.ok(TaskDTO.create(task));
+    }
+
+}
