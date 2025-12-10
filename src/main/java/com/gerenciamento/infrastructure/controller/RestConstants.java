ee6f07081d542fee459c58c3ae057bde874c75b5
Tiago Schermack <tiago.l07@aluno.ifsc.edu.br>
2025-12-09 21:24:20 -0300

    chore: adicionar nova rota representando a tarefa

--- src/main/java/com/gerenciamento/infrastructure/controller/RestConstants.java
+++ src/main/java/com/gerenciamento/infrastructure/controller/RestConstants.java
@@ -4,6 +4,7 @@ public final class RestConstants {
 
     public static final String PATH_PROJECTS = "/projects";
     public static final String PATH_MEMBERS  = "/members";
+    public static final String PATH_TASKS    = "/tasks";
 
     private RestConstants() {}
 }
